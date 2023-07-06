a5f38f6e3f1bbae9715253f85c81faa342704d3f
x1ss <shaleprog@gmail.com>
2023-07-06 22:24:40 +0300

    add user input

--- src/main/java/by/x1ss/Main.java
+++ src/main/java/by/x1ss/Main.java
@@ -4,6 +4,7 @@ import by.x1ss.calculator.Calculator;
 import by.x1ss.calculator.Money;
 
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main
 {
@@ -15,22 +16,32 @@ public class Main
             throw new RuntimeException(e.getMessage());
         }
 
-        try {
-            System.out.println(calculator.calculate("toDollars(-100р + (100р - 200р + 50р) + 1р)"));
-        } catch (NumberFormatException e){
-            boolean checker = true;
-            for (var money : Money.values()) {
-                if (e.getMessage().contains(String.valueOf(money.symbol))) {
-                    System.out.println("Wrong operation " + money.name() + " value. Error of converting currency: different currencies in one addition or subtraction operation. ");
-                    checker = false;
+
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.println("Enter expression (enter exit to close application):");
+            String expression = scanner.nextLine();
+            if(expression.equals("exit")){
+                break;
+            }
+            try {
+                System.out.println(calculator.calculate(expression));
+            } catch (NumberFormatException e){
+                boolean checker = true;
+                for (var money : Money.values()) {
+                    if (e.getMessage().contains(String.valueOf(money.symbol))) {
+                        System.out.println("Wrong operation " + money.name() + " value. Error of converting currency: different currencies in one addition or subtraction operation. ");
+                        checker = false;
+                    }
                 }
+                if(checker){
+                    System.out.println("Wrong operation: " + e.getMessage().charAt(10));
+                }
+            } catch (Exception e){
+                System.out.println(e.getMessage());
             }
-            if(checker){
-                System.out.println("Wrong operation: " + e.getMessage().charAt(10));
-            }
-        } catch (Exception e){
-            System.out.println(e.getMessage());
         }
+
 
     }
 }
