54120c641fed93fad160a646ff7906265ba0b45e
x1ss <shaleprog@gmail.com>
2023-07-10 20:25:12 +0300

    edit add builder

--- src/main/java/by/x1ss/ModsenPractice/entity/ExchangeRate.java
+++ src/main/java/by/x1ss/ModsenPractice/entity/ExchangeRate.java
@@ -1,10 +1,14 @@
-package by.x1ss.entity;
+package by.x1ss.ModsenPractice.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@AllArgsConstructor
+@Builder
 public class ExchangeRate {
     private final String baseCurrency;
     private final String exchangeCurrency;
