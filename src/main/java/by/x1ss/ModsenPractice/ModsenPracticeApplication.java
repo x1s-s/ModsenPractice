fd0f531d0584ba8fce1605833977b8a6abebddb6
x1ss <shaleprog@gmail.com>
2023-07-10 20:24:26 +0300

    add Spring boot

---
+++ src/main/java/by/x1ss/ModsenPractice/ModsenPracticeApplication.java
@@ -0,0 +1,13 @@
+package by.x1ss.ModsenPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModsenPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModsenPracticeApplication.class, args);
+	}
+
+}
