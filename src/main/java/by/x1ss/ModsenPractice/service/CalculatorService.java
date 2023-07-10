89638794694b338436835f94943b600efea7556a
x1ss <shaleprog@gmail.com>
2023-07-10 20:25:26 +0300

    add CalculatorService

---
+++ src/main/java/by/x1ss/ModsenPractice/service/CalculatorService.java
@@ -0,0 +1,10 @@
+package by.x1ss.ModsenPractice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+    public String calculate(String expression) {
+        return "1";
+    }
+}
