6f2eb6051e200afe840892d2654ae2b650fa4ff2
x1ss <shaleprog@gmail.com>
2023-07-11 22:27:46 +0300

    edit write calculate method

--- src/main/java/by/x1ss/ModsenPractice/service/CalculatorService.java
+++ src/main/java/by/x1ss/ModsenPractice/service/CalculatorService.java
@@ -1,10 +1,22 @@
 package by.x1ss.ModsenPractice.service;
 
+import by.x1ss.ModsenPractice.calculator.Calculator;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class CalculatorService {
+    private final Calculator calculator;
+
     public String calculate(String expression) {
-        return "1";
+        try{
+            return calculator.calculate(expression);
+        } catch (Exception e){
+            log.warn(e.getMessage());
+            return null;
+        }
     }
 }
