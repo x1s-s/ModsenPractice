b6c51ebf2f3e7573fc3e589d9d051a35cfb7d900
x1ss <shaleprog@gmail.com>
2023-07-13 21:08:46 +0300

    edit add logging

--- src/main/java/by/x1ss/ModsenPractice/controllers/CalculatorController.java
+++ src/main/java/by/x1ss/ModsenPractice/controllers/CalculatorController.java
@@ -2,11 +2,13 @@ package by.x1ss.ModsenPractice.controllers;
 
 import by.x1ss.ModsenPractice.service.CalculatorService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
+@Slf4j
 @RequiredArgsConstructor
 public class CalculatorController {
     private final CalculatorService calculatorService;
@@ -14,6 +16,7 @@ public class CalculatorController {
     @GetMapping("/calculate")
     @ResponseStatus(HttpStatus.OK)
     public String calculate(@RequestBody String expression) {
+        log.info("Calculate expression: {}", expression);
         return calculatorService.calculate(expression);
     }
 
