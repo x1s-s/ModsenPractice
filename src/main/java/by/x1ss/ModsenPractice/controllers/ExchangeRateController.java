fd958eaf811c3c3057e24369a6a4040eb47553f2
x1ss <shaleprog@gmail.com>
2023-07-13 21:05:54 +0300

    add ExchangeRateDto

--- src/main/java/by/x1ss/ModsenPractice/controllers/ExchangeRateController.java
+++ src/main/java/by/x1ss/ModsenPractice/controllers/ExchangeRateController.java
@@ -2,6 +2,7 @@ package by.x1ss.ModsenPractice.controllers;
 
 import by.x1ss.ModsenPractice.service.CurrencyConvertorService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +10,7 @@ import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("api/v1/exchangeRates")
+@Slf4j
 @RequiredArgsConstructor
 public class ExchangeRateController {
     private final CurrencyConvertorService currencyConvertorService;
@@ -16,6 +18,7 @@ public class ExchangeRateController {
     @GetMapping("/{baseCurrency}/{exchangeCurrency}")
     @ResponseStatus(HttpStatus.OK)
     public BigDecimal getCurrency(@PathVariable String baseCurrency, @PathVariable String exchangeCurrency){
+        log.info("Get exchange rate for {} to {}", baseCurrency, exchangeCurrency);
         return currencyConvertorService.getExchangeRate(baseCurrency,exchangeCurrency);
     }
 }
