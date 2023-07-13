c3c2435c5817496301d2cf3564b53614339adc34
x1ss <shaleprog@gmail.com>
2023-07-13 21:07:39 +0300

    refactor code

--- src/main/java/by/x1ss/ModsenPractice/exception/IllegalOperation.java
+++ src/main/java/by/x1ss/ModsenPractice/exception/IllegalOperation.java
@@ -1,6 +1,6 @@
 package by.x1ss.ModsenPractice.exception;
 
-public class IllegalOperation extends RuntimeException{
+public class IllegalOperation extends RuntimeException {
     @Override
     public String getMessage() {
         return "Can't find operation or operation is incorrect";
