06dd676511564016a4bcf1326d1153c54b7794c9
x1ss <shaleprog@gmail.com>
2023-07-14 21:18:01 +0300

    edit add cachemanager field and cache check

--- src/main/java/by/x1ss/ModsenPractice/cache/ExchangeRateCacheManager.java
+++ src/main/java/by/x1ss/ModsenPractice/cache/ExchangeRateCacheManager.java
@@ -4,25 +4,35 @@ import by.x1ss.ModsenPractice.service.ExchangeRateGetService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class ExchangeRateCacheManager {
     private final ExchangeRateGetService exchangeRateGetService;
+    private final CacheManager cacheManager;
 
     @CacheEvict(value = "exchangeRates", allEntries = true)
     public void clearCache() {
-        log.info("Cache cleared");
+        if(cacheManager.getCacheNames().isEmpty()){
+            log.info("Cache cleared");
+        } else {
+            log.warn("Cache not cleared");
+        }
     }
 
     @EventListener(ApplicationReadyEvent.class)
     public void startLoadCache() {
-        log.info("Cache loaded : {}", exchangeRateGetService.getExchangeRates());
+        exchangeRateGetService.getExchangeRates();
+        log.info("Cache loaded : {}", Objects.requireNonNull(
+                cacheManager.getCache("exchangeRates")).getNativeCache());
     }
 
     @Scheduled(cron = "0 0 0 * * *")
