fdfb33c82635a7b3f001e1461ebf7f88d27bbe0f
x1ss <shaleprog@gmail.com>
2023-07-16 01:33:15 +0300

    add AbstractTest class

--- src/test/java/by/x1ss/ModsenPractice/AbstractTest.java
+++ src/test/java/by/x1ss/ModsenPractice/AbstractTest.java
@@ -1,7 +1,9 @@
 package by.x1ss.ModsenPractice;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 public class AbstractTest {
 }
