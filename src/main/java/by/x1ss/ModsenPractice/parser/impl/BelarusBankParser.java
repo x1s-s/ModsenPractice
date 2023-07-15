6cb99c0a0a3e6fe4c3f33dad441ec886553f145a
x1ss <shaleprog@gmail.com>
2023-07-16 01:38:10 +0300

    edit try catch brackets

--- src/main/java/by/x1ss/ModsenPractice/parser/impl/BelarusBankParser.java
+++ src/main/java/by/x1ss/ModsenPractice/parser/impl/BelarusBankParser.java
@@ -3,7 +3,6 @@ package by.x1ss.ModsenPractice.parser.impl;
 import by.x1ss.ModsenPractice.dto.ExchangeRateDto;
 import by.x1ss.ModsenPractice.exception.ResponseParseException;
 import by.x1ss.ModsenPractice.parser.Parser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -23,66 +22,66 @@ public class BelarusBankParser implements Parser {
 
     @Override
     public List<ExchangeRateDto> parse(String input) {
-        List<ExchangeRateDto> exchangeRateDtos = new ArrayList<>();
-        input = input.substring(input.indexOf("{"), input.lastIndexOf("}") + 1);
-        log.info("Parsing input: {}", input);
         JsonNode jsonNode;
         try {
+            input = input.substring(input.indexOf("{"), input.lastIndexOf("}") + 1);
+            log.info("Parsing input: {}", input);
             jsonNode = objectMapper.readTree(input);
-        } catch (JsonProcessingException e) {
+            List<ExchangeRateDto> exchangeRateDtos = new ArrayList<>();
+            exchangeRateDtos.add(
+                    ExchangeRateDto.builder()
+                            .baseCurrency("USD")
+                            .exchangeCurrency("BYN")
+                            .exchangeRate(new BigDecimal(jsonNode.get("USD_in").asText().replace("\"", "")))
+                            .build()
+            );
+            exchangeRateDtos.add(
+                    ExchangeRateDto.builder()
+                            .baseCurrency("BYN")
+                            .exchangeCurrency("USD")
+                            .exchangeRate(BigDecimal.ONE
+                                    .divide(
+                                            new BigDecimal(jsonNode.get("USD_out").asText().replace("\"", "")),
+                                            10, RoundingMode.FLOOR))
+                            .build()
+            );
+            exchangeRateDtos.add(
+                    ExchangeRateDto.builder()
+                            .baseCurrency("EUR")
+                            .exchangeCurrency("BYN")
+                            .exchangeRate(new BigDecimal(jsonNode.get("EUR_in").asText().replace("\"", "")))
+                            .build()
+            );
+            exchangeRateDtos.add(
+                    ExchangeRateDto.builder()
+                            .baseCurrency("BYN")
+                            .exchangeCurrency("EUR")
+                            .exchangeRate(BigDecimal.ONE
+                                    .divide(
+                                            new BigDecimal(jsonNode.get("EUR_out").asText().replace("\"", "")),
+                                            10, RoundingMode.FLOOR))
+                            .build()
+            );
+            exchangeRateDtos.add(
+                    ExchangeRateDto.builder()
+                            .baseCurrency("USD")
+                            .exchangeCurrency("EUR")
+                            .exchangeRate(new BigDecimal(jsonNode.get("USD_EUR_in").asText().replace("\"", "")))
+                            .build()
+            );
+            exchangeRateDtos.add(
+                    ExchangeRateDto.builder()
+                            .baseCurrency("EUR")
+                            .exchangeCurrency("USD")
+                            .exchangeRate(BigDecimal.ONE.divide(
+                                    new BigDecimal(jsonNode.get("USD_EUR_out").asText().replace("\"", "")),
+                                    10, RoundingMode.FLOOR))
+                            .build()
+            );
+            log.info("Parsed exchange rates: {}", exchangeRateDtos);
+            return exchangeRateDtos;
+        } catch (Exception e) {
             throw new ResponseParseException(input);
         }
-        exchangeRateDtos.add(
-                ExchangeRateDto.builder()
-                        .baseCurrency("USD")
-                        .exchangeCurrency("BYN")
-                        .exchangeRate(new BigDecimal(jsonNode.get("USD_in").asText().replace("\"", "")))
-                        .build()
-        );
-        exchangeRateDtos.add(
-                ExchangeRateDto.builder()
-                        .baseCurrency("BYN")
-                        .exchangeCurrency("USD")
-                        .exchangeRate(BigDecimal.ONE
-                                .divide(
-                                        new BigDecimal(jsonNode.get("USD_out").asText().replace("\"", "")),
-                                        10, RoundingMode.FLOOR))
-                        .build()
-        );
-        exchangeRateDtos.add(
-                ExchangeRateDto.builder()
-                        .baseCurrency("EUR")
-                        .exchangeCurrency("BYN")
-                        .exchangeRate(new BigDecimal(jsonNode.get("EUR_in").asText().replace("\"", "")))
-                        .build()
-        );
-        exchangeRateDtos.add(
-                ExchangeRateDto.builder()
-                        .baseCurrency("BYN")
-                        .exchangeCurrency("EUR")
-                        .exchangeRate(BigDecimal.ONE
-                                .divide(
-                                        new BigDecimal(jsonNode.get("EUR_out").asText().replace("\"", "")),
-                                        10, RoundingMode.FLOOR))
-                        .build()
-        );
-        exchangeRateDtos.add(
-                ExchangeRateDto.builder()
-                        .baseCurrency("USD")
-                        .exchangeCurrency("EUR")
-                        .exchangeRate(new BigDecimal(jsonNode.get("USD_EUR_in").asText().replace("\"", "")))
-                        .build()
-        );
-        exchangeRateDtos.add(
-                ExchangeRateDto.builder()
-                        .baseCurrency("EUR")
-                        .exchangeCurrency("USD")
-                        .exchangeRate(BigDecimal.ONE.divide(
-                                new BigDecimal(jsonNode.get("USD_EUR_out").asText().replace("\"", "")),
-                                10, RoundingMode.FLOOR))
-                        .build()
-        );
-        log.info("Parsed exchange rates: {}", exchangeRateDtos);
-        return exchangeRateDtos;
     }
 }
