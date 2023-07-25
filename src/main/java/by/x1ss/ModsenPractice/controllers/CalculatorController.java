8172aae31f2a52e5815f293d324f8fb3a41e6edf
x1ss <shaleprog@gmail.com>
2023-07-25 20:01:27 +0300

    edit split imports

--- src/main/java/by/x1ss/ModsenPractice/controllers/CalculatorController.java
+++ src/main/java/by/x1ss/ModsenPractice/controllers/CalculatorController.java
@@ -4,7 +4,11 @@ import by.x1ss.ModsenPractice.service.CalculatorService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
